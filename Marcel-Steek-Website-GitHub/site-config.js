4d65aa7a8897e737b5ae29a4fb9dd843035ff5a7
95nnk99r9h-rgb <95nnk99r9h@privaterelay.appleid.com>
2026-09-18 13:49:22 +0200

    Delete Marcel-Steek-Website-GitHub directory

--- Marcel-Steek-Website-GitHub/site-config.js
+++
@@ -1,7 +0,0 @@
-/** ZENTRALE KONTAKT-EINSTELLUNGEN – keine Passwörter oder API-Schlüssel eintragen.
- * Anleitung: README.md. Änderungen der Service-Worker-Version in sw.js mitziehen.
- */
-window.SITE_CONFIG = Object.freeze({
-  contactEmail: "", // Echte Empfängeradresse, z. B. die von Marcel bereitgestellte Adresse.
-  formEndpoint: "" // Optional: öffentlicher HTTPS-Formular-Endpunkt (Formspree-kompatibel).
-});
